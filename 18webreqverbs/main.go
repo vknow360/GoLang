b8876a50847821463180a32bd5907cea9911a4be
Sunny Gupta <vknow360@gmail.com>
2025-01-23 20:18:59 +0530

    day4

--- 18webreqverbs/main.go
+++ 18webreqverbs/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
-const url = "http://localhost:8000"
+const murl = "http://localhost:8000/post"
 
 func main() {
-	PerformGet(url)
+	//PerformGet(murl)
+	//PerformJsonPostRequest()
+	PerformFormDataPostRequest()
 }
 
 func PerformGet(url string) {
@@ -24,4 +28,41 @@ func PerformGet(url string) {
 
 	content, _ := io.ReadAll(response.Body)
 	fmt.Println(string(content))
+}
+
+func PerformJsonPostRequest() {
+	requestBody := strings.NewReader(`
+		{
+			"courseName" : "Let's go with GoLang",
+			"price": 0,
+			"platform" : "Youtube"
+		}
+	`)
+
+	response, err := http.Post(murl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+}
+
+func PerformFormDataPostRequest() {
+	const myurl = "http://localhost:8000/postform"
+
+	data := url.Values{}
+	data.Add("firstname", "Sunny")
+	data.Add("lastname", "Gupta")
+	data.Add("email", "vknow360@gmail.com")
+
+	response, err := http.PostForm(murl, data)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := io.ReadAll(response.Body)
+	fmt.Println(string(content))
+
 }
---
+++ 20buildapi/go.mod
@@ -0,0 +1,5 @@
+module github.com/vknow360/buildapi
+
+go 1.23.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
